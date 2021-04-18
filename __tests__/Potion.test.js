ae67c5ea69964ea9bcf61a901b268a3349ef0506
Stevtm <steventmaddison@outlook.com>
2021-04-18 15:34:07 -0400

    fail Potion test

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,9 @@
+const { TestScheduler } = require("@jest/core");
+const Potion = require("../lib/Potion");
+
+test("creates a health potion object", () => {
+	const potion = new Potion("health");
+
+	expect(potion.name).toBe("health");
+	expect(potion.value).toEqual(expect.any(Number));
+});
