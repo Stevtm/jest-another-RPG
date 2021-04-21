862105750f02879ceabe03f768346e9b4b012a4b
Stevtm <steventmaddison@outlook.com>
2021-04-20 20:15:34 -0400

    test and add isAlive() and reduceHealth() Player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -42,3 +42,26 @@ test("gets player's health value", () => {
 		expect.stringContaining(player.health.toString())
 	);
 });
+
+test("checks if the player is alive or not", () => {
+	const player = new Player();
+
+	expect(player.isAlive()).toBeTruthy();
+
+	player.health = 0;
+
+	expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+	const player = new Player();
+	const oldHealth = player.health;
+
+	player.reduceHealth(5);
+
+	expect(player.health).toBe(oldHealth - 5);
+
+	player.reduceHealth(99999);
+
+	expect(player.health).toBe(0);
+});
