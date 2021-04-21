1c7fdf482731b735eaeae4e78a19b7b51348addb
Stevtm <steventmaddison@outlook.com>
2021-04-20 20:47:49 -0400

    test and add usePotion() Player method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -80,5 +80,15 @@ test("adds a potion to the inventory", () => {
 
 	player.addPotion(new Potion());
 
-	expect(player.inventory.lenth).toBeGreaterThan(oldCount);
+	expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test("uses a potion from inventory", () => {
+	const player = new Player("Dave");
+	player.inventory = [new Potion(), new Potion(), new Potion()];
+	const oldCount = player.inventory.length;
+
+	player.usePotion(1);
+
+	expect(player.inventory.length).toBeLessThan(oldCount);
 });
